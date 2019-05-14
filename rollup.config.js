049e30edf2b4d41933476f40884414407bf9aced
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2019-05-14 22:27:15 +0800

    🔧 rollup: Update build to not mangle public classes, fix #3

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ import typescript from 'rollup-plugin-typescript2';
 
 const isProd = !process.env.ROLLUP_WATCH;
 const input = ['src/polling-observer.ts'];
-const pluginFn = (iife, minify) => [
+const pluginFn = (format, minify) => [
   isProd && tslint({
     throwError: true,
     configuration: `tslint${isProd ? '.prod' : ''}.json`,
@@ -15,9 +15,17 @@ const pluginFn = (iife, minify) => [
   typescript({
     tsconfig: './tsconfig.json',
     exclude: isProd ? ['src/(demo|test)/**/*'] : [],
-    ...(iife ? { tsconfigOverride: { compilerOptions: { target: 'es5' } } } : {}),
+    ...('iife' === format ? { tsconfigOverride: { compilerOptions: { target: 'es5' } } } : {}),
   }),
-  isProd && minify && terser(),
+  isProd && minify && terser({
+    mangle: {
+      properties: { regex: /^_/ },
+      reserved: ['PollingMeasure', 'PollingObserver'],
+      safari10: true,
+      toplevel: true,
+      module: 'esm' === format,
+    },
+  }),
   isProd && filesize({ showBrotliSize: true }),
 ];
 
@@ -50,7 +58,7 @@ const multiBuild = [
   const opts = [true, false].map(o => ({
     input,
     output: o ? { ...n, file: n.file.replace(/(.+)(\.m?js)$/, '$1.min$2') } : n,
-    plugins: pluginFn('iife' === n.format, o),
+    plugins: pluginFn(n.format, o),
   }));
 
   return (p.push(...opts), p);
